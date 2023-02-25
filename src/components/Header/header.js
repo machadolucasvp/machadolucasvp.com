8ab579a74dae469c3ad74f7119e65bf14a1be880
Lucas Machado <machadolucasvp@gmail.com>
2023-02-25 15:45:21 +0100

    feat: add google analytics

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,100 +1,109 @@
-import React, { useEffect } from 'react';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
-import Typist from 'react-typist';
-import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect } from "react"
+import ReactGA from "react-ga"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import Typist from "react-typist"
+import { faLightbulb } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import profile from '../../assets/profile.jpeg';
-import headerStyles from './header.module.scss';
-import { useTheme } from '../../contexts/theme';
+import profile from "../../assets/profile.jpeg"
+import headerStyles from "./header.module.scss"
+import { useTheme } from "../../contexts/theme"
 
-const buildBulbColor = (themeType) => (!isDarkMode(themeType) ? '#ffbd69' : '#ffffff');
-const isDarkMode = (themeType) => themeType === 'dark';
+const { GA_TOKEN } = process.env
+
+const buildBulbColor = themeType =>
+    !isDarkMode(themeType) ? "#ffbd69" : "#ffffff"
+const isDarkMode = themeType => themeType === "dark"
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme()
 
-  useEffect(() => {
-    setTheme(window.__theme);
-    window.__onThemeChange = () => setTheme(window.__theme);
-  }, []);
+    useEffect(() => {
+        setTheme(window.__theme)
+        window.__onThemeChange = () => setTheme(window.__theme)
+        ReactGA.initialize(GA_TOKEN)
+    }, [])
 
-  const handleBulbClick = () => {
-    window.__setPreferredTheme(isDarkMode(theme.type) ? 'light' : 'dark');
-    setTheme(window.__theme);
-    if (window.DISQUS !== undefined) {
-      window.setTimeout(() => {
-        window.DISQUS.reset({
-          reload: true,
-        });
-      }, 300);
+    const handleBulbClick = () => {
+        window.__setPreferredTheme(isDarkMode(theme.type) ? "light" : "dark")
+        setTheme(window.__theme)
+        if (window.DISQUS !== undefined) {
+            window.setTimeout(() => {
+                window.DISQUS.reset({
+                    reload: true,
+                })
+            }, 300)
+        }
     }
-  };
 
-  return (
-    <header>
-      <nav className={headerStyles.nav}>
-        <div className={headerStyles.profileCard}>
-          <img src={profile} alt="Profile" />
-          <AniLink
-            className={headerStyles.listItemLink}
-            paintDrip
-            direction="top"
-            duration={0.7}
-            to="/"
-            hex={theme.color}
-          >
-            {' '}
-            <Typist className={headerStyles.typer} cursor={{ hideWhenDone: true }} startDelay={1000}>
-                <h2>/machadolucasvp</h2>
-            </Typist>
-          </AniLink>
-        </div>
-        <ul className={headerStyles.list}>
-          <li className={headerStyles.listItem}>
-            <AniLink
-              className={headerStyles.listItemLink}
-              activeClassName={headerStyles.activeListItem}
-              paintDrip
-              direction="top"
-              duration={0.7}
-              to="/"
-              hex={theme.color}
-            >
-              About
-            </AniLink>
-          </li>
-          <li className={headerStyles.listItem}>
-            <AniLink
-              className={headerStyles.listItemLink}
-              activeClassName={headerStyles.activeListItem}
-              paintDrip
-              direction="top"
-              duration={0.7}
-              to="/articles"
-              hex={theme.color}
-            >
-              Articles
-            </AniLink>
-          </li>
-          <li className={headerStyles.listItem}>
-            <button
-              className={headerStyles.bulbButton}
-              onClick={handleBulbClick}
-              type="button"
-            >
-              <FontAwesomeIcon
-                id="bulb"
-                icon={faLightbulb}
-                size="sm"
-                color={buildBulbColor(theme.type)}
-              />
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <header>
+            <nav className={headerStyles.nav}>
+                <div className={headerStyles.profileCard}>
+                    <img src={profile} alt="Profile" />
+                    <AniLink
+                        className={headerStyles.listItemLink}
+                        paintDrip
+                        direction="top"
+                        duration={0.7}
+                        to="/"
+                        hex={theme.color}
+                    >
+                        {" "}
+                        <Typist
+                            className={headerStyles.typer}
+                            cursor={{ hideWhenDone: true }}
+                            startDelay={1000}
+                        >
+                            <h2>/machadolucasvp</h2>
+                        </Typist>
+                    </AniLink>
+                </div>
+                <ul className={headerStyles.list}>
+                    <li className={headerStyles.listItem}>
+                        <AniLink
+                            className={headerStyles.listItemLink}
+                            activeClassName={headerStyles.activeListItem}
+                            paintDrip
+                            direction="top"
+                            duration={0.7}
+                            to="/"
+                            hex={theme.color}
+                        >
+                            About
+                        </AniLink>
+                    </li>
+                    <li className={headerStyles.listItem}>
+                        <AniLink
+                            className={headerStyles.listItemLink}
+                            activeClassName={headerStyles.activeListItem}
+                            paintDrip
+                            direction="top"
+                            duration={0.7}
+                            to="/articles"
+                            hex={theme.color}
+                        >
+                            Articles
+                        </AniLink>
+                    </li>
+                    <li className={headerStyles.listItem}>
+                        <button
+                            className={headerStyles.bulbButton}
+                            onClick={handleBulbClick}
+                            type="button"
+                        >
+                            <FontAwesomeIcon
+                                id="bulb"
+                                icon={faLightbulb}
+                                size="sm"
+                                color={buildBulbColor(theme.type)}
+                            />
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
